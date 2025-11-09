0a93e12b5f2a3804e5ca7cd6d7b1e3fae84a64b9
BjossiAlfreds <bjornalfredsson@yahoo.com>
2025-11-09 02:13:24 +0000

    menu: GetRateSelectionIndex function and optimized rate_tbl search

--- src/client/menu/menu.c
+++ src/client/menu/menu.c
@@ -6006,9 +6006,11 @@ HandednessCallback(void *unused)
 static void
 RateCallback(void *unused)
 {
-	if (s_player_rate_box.curvalue != sizeof(rate_tbl) / sizeof(*rate_tbl) - 1)
+	int i = s_player_rate_box.curvalue;
+
+	if (i < (sizeof(rate_tbl) / sizeof(*rate_tbl)))
 	{
-		Cvar_SetValue("rate", (float)rate_tbl[s_player_rate_box.curvalue]);
+		Cvar_SetValue("rate", rate_tbl[i]);
 	}
 }
 
@@ -6525,6 +6527,26 @@ ListModels_f(void)
 	PlayerModelFree();
 }
 
+static int
+GetRateSelectionIndex(void)
+{
+	int i, curvalue;
+
+	curvalue = Cvar_VariableValue("rate");
+
+	for (i = 0; i < (sizeof(rate_tbl) / sizeof(*rate_tbl)); i++)
+	{
+		if (curvalue == rate_tbl[i])
+		{
+			break;
+		}
+	}
+
+	/* i == rate_tbl length is "User defined" */
+
+	return i;
+}
+
 static qboolean
 PlayerConfig_MenuInit(void)
 {
@@ -6536,7 +6558,6 @@ PlayerConfig_MenuInit(void)
 	char imgname[MAX_QPATH];
 	int mdlindex = 0;
 	int imgindex = 0;
-	int i = 0;
 	float scale = SCR_GetMenuScale();
 
 	if (PlayerConfig_ScanDirectories() == false)
@@ -6647,14 +6668,6 @@ PlayerConfig_MenuInit(void)
 	s_player_handedness_box.curvalue = ClampCvar(0, 2, hand->value);
 	s_player_handedness_box.itemnames = handedness;
 
-	for (i = 0; i < sizeof(rate_tbl) / sizeof(*rate_tbl) - 1; i++)
-	{
-		if (Cvar_VariableValue("rate") == rate_tbl[i])
-		{
-			break;
-		}
-	}
-
 	s_player_rate_title.generic.type = MTYPE_SEPARATOR;
 	s_player_rate_title.generic.name = "connect speed";
 	s_player_rate_title.generic.x = 56 * scale;
@@ -6666,7 +6679,7 @@ PlayerConfig_MenuInit(void)
 	s_player_rate_box.generic.name = NULL;
 	s_player_rate_box.generic.cursor_offset = -48;
 	s_player_rate_box.generic.callback = RateCallback;
-	s_player_rate_box.curvalue = i;
+	s_player_rate_box.curvalue = GetRateSelectionIndex();
 	s_player_rate_box.itemnames = rate_names;
 
 	s_player_download_action.generic.type = MTYPE_ACTION;
