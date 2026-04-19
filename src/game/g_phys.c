3d36e2d3962426793de8ce3760579ec9c2af2a30
BjossiAlfreds <bjornalfredsson@yahoo.com>
2026-04-19 17:24:39 +0000

    game: Prevent SV_Push from corrupting memory
    This commit also does a bit of refactoring

--- src/game/g_phys.c
+++ src/game/g_phys.c
@@ -588,24 +588,65 @@ typedef struct
 } pushed_t;
 
 static pushed_t pushed[MAX_EDICTS], *pushed_p;
-static edict_t *obstacle;
+
+static pushed_t *
+pushed_append(edict_t *e)
+{
+	if (pushed_p >= ARREND(pushed))
+	{
+		return NULL;
+	}
+
+	pushed_p->ent = e;
+	VectorCopy(e->s.origin, pushed_p->origin);
+	VectorCopy(e->s.angles, pushed_p->angles);
+
+	return pushed_p++;
+}
+
+static void
+pushed_pop(void)
+{
+	if (pushed_p > pushed)
+	{
+		pushed_p--;
+	}
+}
+
+static void
+pushed_undo(void)
+{
+	pushed_t *p;
+
+	/* go backwards, so if the same entity was pushed
+	   twice, it goes back to the original position */
+	for (p = pushed_p - 1; p >= pushed; p--)
+	{
+		VectorCopy(p->origin, p->ent->s.origin);
+		VectorCopy(p->angles, p->ent->s.angles);
+
+		gi.linkentity(p->ent);
+	}
+
+	pushed_p = pushed;
+}
 
 /*
  * Objects need to be moved back on a failed push,
  * otherwise riders would continue to slide.
  */
-static qboolean
+static edict_t *
 SV_Push(edict_t *pusher, vec3_t move, vec3_t amove)
 {
-	int i, e;
-	edict_t *check, *block;
+	int i;
+	edict_t *check;
 	pushed_t *p;
 	vec3_t org, org2, move2, forward, right, up;
 	vec3_t realmins, realmaxs;
 
 	if (!pusher)
 	{
-		return false;
+		return NULL;
 	}
 
 	/* clamp the move to 1/8 units, so the position will
@@ -632,10 +673,10 @@ SV_Push(edict_t *pusher, vec3_t move, vec3_t amove)
 	AngleVectors(org, forward, right, up);
 
 	/* save the pusher's original position */
-	pushed_p->ent = pusher;
-	VectorCopy(pusher->s.origin, pushed_p->origin);
-	VectorCopy(pusher->s.angles, pushed_p->angles);
-	pushed_p++;
+	if (!pushed_append(pusher))
+	{
+		return NULL;
+	}
 
 	/* move the pusher to it's final position */
 	VectorAdd(pusher->s.origin, move, pusher->s.origin);
@@ -648,9 +689,7 @@ SV_Push(edict_t *pusher, vec3_t move, vec3_t amove)
 
 	/* see if any solid entities
 	   are inside the final position */
-	check = g_edicts + 1;
-
-	for (e = 1; e < globals.num_edicts; e++, check++)
+	for (check = g_edicts + 1; check < &g_edicts[globals.num_edicts]; check++)
 	{
 		if (!check->inuse)
 		{
@@ -696,11 +735,10 @@ SV_Push(edict_t *pusher, vec3_t move, vec3_t amove)
 		if ((pusher->movetype == MOVETYPE_PUSH) ||
 			(check->groundentity == pusher))
 		{
-			/* move this entity */
-			pushed_p->ent = check;
-			VectorCopy(check->s.origin, pushed_p->origin);
-			VectorCopy(check->s.angles, pushed_p->angles);
-			pushed_p++;
+			if (!pushed_append(check))
+			{
+				continue;
+			}
 
 			/* try moving the contacted entity */
 			VectorAdd(check->s.origin, move, check->s.origin);
@@ -719,9 +757,7 @@ SV_Push(edict_t *pusher, vec3_t move, vec3_t amove)
 				check->groundentity = NULL;
 			}
 
-			block = SV_TestEntityPosition(check);
-
-			if (!block)
+			if (!SV_TestEntityPosition(check))
 			{
 				/* pushed ok */
 				gi.linkentity(check);
@@ -734,31 +770,18 @@ SV_Push(edict_t *pusher, vec3_t move, vec3_t amove)
 			   this is only relevent for riding entities, not
 			   pushed */
 			VectorSubtract(check->s.origin, move, check->s.origin);
-			block = SV_TestEntityPosition(check);
 
-			if (!block)
+			if (!SV_TestEntityPosition(check))
 			{
-				pushed_p--;
+				pushed_pop();
 				continue;
 			}
 		}
 
-		/* save off the obstacle so we can
-		   call the block function */
-		obstacle = check;
+		/* move back any entities we already moved */
+		pushed_undo();
 
-		/* move back any entities we already moved
-		   go backwards, so if the same entity was pushed
-		   twice, it goes back to the original position */
-		for (p = pushed_p - 1; p >= pushed; p--)
-		{
-			VectorCopy(p->origin, p->ent->s.origin);
-			VectorCopy(p->angles, p->ent->s.angles);
-
-			gi.linkentity(p->ent);
-		}
-
-		return false;
+		return check;
 	}
 
 	/* see if anything we moved has touched a trigger */
@@ -767,7 +790,7 @@ SV_Push(edict_t *pusher, vec3_t move, vec3_t amove)
 		G_TouchTriggers(p->ent);
 	}
 
-	return true;
+	return NULL;
 }
 
 /*
@@ -777,8 +800,7 @@ SV_Push(edict_t *pusher, vec3_t move, vec3_t amove)
 static void
 SV_Physics_Pusher(edict_t *ent)
 {
-	vec3_t move, amove;
-	edict_t *part, *mv;
+	edict_t *part, *mv, *obstacle;
 
 	if (!ent)
 	{
@@ -796,27 +818,26 @@ SV_Physics_Pusher(edict_t *ent)
 	   any moves or calling any think functions if the move
 	   is blocked, all moved objects will be backed out */
 	pushed_p = pushed;
+	obstacle = NULL;
 
 	for (part = ent; part; part = part->teamchain)
 	{
-		if (part->velocity[0] || part->velocity[1] || part->velocity[2] ||
-			part->avelocity[0] || part->avelocity[1] || part->avelocity[2])
+		vec3_t move, amove;
+
+		if (VectorCompare(part->velocity, vec3_origin) &&
+			VectorCompare(part->avelocity, vec3_origin))
 		{
-			/* object is moving */
-			VectorScale(part->velocity, FRAMETIME, move);
-			VectorScale(part->avelocity, FRAMETIME, amove);
-
-			if (!SV_Push(part, move, amove))
-			{
-				break; /* move was blocked */
-			}
+			continue;
 		}
-	}
 
-	if (pushed_p > &pushed[MAX_EDICTS - 1])
-	{
-		gi.error("pushed_p > &pushed[MAX_EDICTS - 1], memory corrupted");
-		return;
+		VectorScale(part->velocity, FRAMETIME, move);
+		VectorScale(part->avelocity, FRAMETIME, amove);
+
+		obstacle = SV_Push(part, move, amove);
+		if (obstacle)
+		{
+			break;
+		}
 	}
 
 	if (part)
